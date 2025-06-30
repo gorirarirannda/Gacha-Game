e17ec46640278e08eccce640945cfe5637077efa
gorirarirannda <124478779+gorirarirannda@users.noreply.github.com>
2025-06-30 10:41:43 +0900

    add:キャラガチャの変数を記述

---
+++ character.py
@@ -0,0 +1,24 @@
+gacha_count_all = 0 #ガチャ総引き数
+gacha_count_5star = 0 #天井までの引き数
+gacha_count_4star = 0 #星4天井までの引き数
+lose_5star = 0 #すり抜けたら1
+win_5star = 0 #掴みし明光発動なら1
+count_win_5star = 0 #すり抜けなかったPU☆5の数
+count_lose_5star = 0 #すり抜け☆5の数
+count_after_5star = 0 #すり抜け後のPU☆5の数
+count_4star = 0 #星4の数
+
+'''ガチャ結果:0=☆3 1=☆4 2=すり抜け無しPU☆5 3=すり抜け☆5 4=すり抜け後PU☆5'''
+single = 0 #単発の結果
+
+ten_first = 0 #10連1個目の結果
+ten_second = 0 #10連2個目の結果
+ten_third = 0 #10連3個目の結果
+ten_fourth = 0 #10連4個目の結果
+ten_fifth = 0 #10連5個目の結果
+ten_sixth = 0 #10連6個目の結果
+ten_seventh = 0 #10連7個目の結果
+ten_eighth = 0 #10連8個目の結果
+ten_ninth = 0 #10連9個目の結果
+ten_tenth = 0 #10連10個目の結果
+
